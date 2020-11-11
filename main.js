c6b070f0a245908b763d545c8dc37dcdddb59e6a
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-11 10:43:29 +0100

    Definita una lista di todo e stampata in pagina in un elenco

---
+++ main.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+
+    el : '#root',
+
+    data : {
+
+        todos : [
+            'item 1',
+            'item 2',
+            'item 3',
+            'item 4',
+            'item 5',
+            'item 6',
+            'item 7'
+        ]
+        
+    },
+
+    methods : {
+
+    }
+
+});
