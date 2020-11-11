2a0c5387b03d2b0046e97f6eec7c16910435d33f
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-11 11:24:01 +0100

    Aggiunti alcuni controlli sull'input dell'utente

--- main.js
+++ main.js
@@ -20,11 +20,26 @@ var app = new Vue({
 
     methods : {
 
+        // this function checks if the user input is valid
+        // in this case, the input is valid if it is not already included in the todo-list and it is not an empty string
+        isInputValid(input) {
+            return !this.todos.includes(input) && input != '';
+        },
+
+        // this function adds a new item to the todo-list (the new item will be a user input)
         addNewItem() {
-            this.todos.push(this.newItem);
+            if (this.isInputValid(this.newItem)) {
+                // if the user input is valid, we add the user input to the todo-list
+                this.todos.push(this.newItem);
+            } else if (this.todos.includes(this.newItem)) {
+                // if the user input already occurs in the todo-list, we notify the user about this fact
+                alert('This item already occurs in the list');
+            }
+            // in any case, we reset the value of the input
             this.newItem = '';
         },
 
+        // this function deletes an item from the todo-list
         deleteItem(index) {
             this.todos.splice(index, 1);
         }
